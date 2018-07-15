944171819314090affdd57c2fbc1d2965941c169
Matthias Kovatsch <github@kovatsch.net>
2018-07-15 22:11:35 +0200

    feat: add webpack for TD tools

---
+++ packages/td-tools/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    mode: "production",
+    entry: "./src/td-tools.ts",
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "node-wot-td-tools.bundle.js",
+        library: "TDTools"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            { test: /\.ts$/, loader: "ts-loader" }
+        ]
+    }
+}
