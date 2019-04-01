a5ea7406011a01c7e0cf1fd61998ec6b99be5400
Hassib Belhaj <hassib.blh@gmail.com>
2019-04-01 13:14:03 +0200

    feat: add browser bundle package
    Signed-off-by: Hassib Belhaj <hassib.blh@gmail.com>

---
+++ packages/browser-bundle/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var Wot = {};
+Wot.Core = require("@node-wot/core");
+Wot.Http = require("@node-wot/binding-http");
+Wot.WebSocket = require("@node-wot/binding-websockets");
+
+if (typeof window !== "undefined") {
+    window.Wot = WoT;
+} else if (typeof module !== "undefined" && module.exports) {
+    module.exports = WoT;
+}
