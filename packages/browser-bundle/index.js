ead47bd9b79da24ca6c790bd1265d0cd828e8e00
Hassib Belhaj <hassib.blh@gmail.com>
2019-04-01 13:44:19 +0200

    fix(browser-bundle): fix typo
    Signed-off-by: Hassib Belhaj <hassib.blh@gmail.com>

--- packages/browser-bundle/index.js
+++ packages/browser-bundle/index.js
@@ -6,7 +6,7 @@ Wot.Http = require("@node-wot/binding-http");
 Wot.WebSocket = require("@node-wot/binding-websockets");
 
 if (typeof window !== "undefined") {
-    window.Wot = WoT;
+    window.Wot = Wot;
 } else if (typeof module !== "undefined" && module.exports) {
-    module.exports = WoT;
+    module.exports = Wot;
 }
