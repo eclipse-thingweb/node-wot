b8dd4fabe7177cdec6d2197c5b7b7ef28a640448
Ege Korkan <egekorkan@gmail.com>
2019-06-12 21:48:33 +0200

    fix: error in description not matching descriptions
    Signed-off-by: Ege Korkan <egekorkan@gmail.com>

--- examples/scripts/counterMultiLang.js
+++ examples/scripts/counterMultiLang.js
@@ -57,7 +57,7 @@ thing.addProperty(
 	NAME_PROPERTY_LAST_CHANGE,
 	{
 		type: "string",
-		description: "increment value",
+		description: "last change of counter value",
 		descriptions: {
 			"en":" last change of counter value",
 			"de": "Letzte Änderung",
