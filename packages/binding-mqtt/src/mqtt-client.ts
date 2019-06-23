9c65da1710150949a8383eeecbdacf3e29005b95
Hassib Belhaj <hassib.blh@gmail.com>
2019-06-23 19:03:27 +0200

    feat(binding-mqtt): update property handling
    Add properties to the mqtt server.
    property op is automatically set to [observeproperty, unobserveproperty].
    Add support for unlinkresource (unsubscribe) to mqtt client.
    
    Signed-off-by: Hassib Belhaj <hassib.blh@gmail.com>

--- packages/binding-mqtt/src/mqtt-client.ts
+++ packages/binding-mqtt/src/mqtt-client.ts
@@ -64,8 +64,7 @@ export default class MqttClient implements ProtocolClient {
             error(error);
         });
 
-
-        return new Subscription(()=>{this.client.end()});
+        return new Subscription(()=>{this.client.unsubscribe(topic)});
       }
 
     
@@ -107,14 +106,25 @@ export default class MqttClient implements ProtocolClient {
 
         });
     }
+
     unlinkResource = (form: Form): Promise<void> => {
-        //TODO: Implement
-        throw new Error('Method not implemented.');
+        let requestUri = url.parse(form['href']);
+        let topic = requestUri.pathname;
+
+        return new Promise<void>((resolve, reject) => {
+            if(this.client && this.client.connected) {
+                this.client.unsubscribe(topic);
+                console.log(`MqttClient unsubscribed from topic '${topic}'`);
+            }
+            resolve()
+        });
     }
+
     start = (): boolean => {
         return true;
     }
     stop = (): boolean => {
+        if(this.client) this.client.end();
         return true;
     }
     
