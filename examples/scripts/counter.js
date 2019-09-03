5f8a5ed659720a0c458f0439aea9711da8b5fbd6
danielpeintner <daniel.peintner@gmail.com>
2019-09-03 14:30:01 +0200

    refactor: emit 0 (=zero) change value when resetting

--- examples/scripts/counter.js
+++ examples/scripts/counter.js
@@ -143,7 +143,7 @@ WoT.produce({
 			console.log("Resetting count");
 			thing.writeProperty("count", 0); 
 			thing.writeProperty("lastChange", (new Date()).toISOString());
-			thing.emitEvent("change");
+			thing.emitEvent("change", 0);
 		});
 		
 		// expose the thing
