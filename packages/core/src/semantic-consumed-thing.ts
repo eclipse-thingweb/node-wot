844e199357d84494030549dadd655d20fe8a0706
danielpeintner <daniel.peintner@gmail.com>
2020-03-16 11:36:16 +0100

    feat: add initial sample for semantic layer of consumed-thing

---
+++ packages/core/src/semantic-consumed-thing.ts
@@ -0,0 +1,51 @@
+/********************************************************************************
+ * Copyright (c) 2018 - 2020 Contributors to the Eclipse Foundation
+ * 
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ * 
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+
+import * as WoT from "wot-typescript-definitions";
+import * as TD from "@node-wot/td-tools";
+
+import { default as ConsumedThing } from "./consumed-thing"
+
+export default class SemanticConsumedThing extends ConsumedThing implements WoT.ConsumedThing {
+
+    /*
+     * Open Questions
+     * - define semantic input
+     *
+     * 
+     */
+
+    readSemanticProperty(semantic: any, options?: WoT.InteractionOptions): Promise<any> {
+        // try to find property name by semantic look-up
+        // e.g., .read( {"@type": "iot:BinarySwitchData"}
+        // walk over available proprties and find (the) right one
+        for (let propertyName in this.properties) {
+            let property: TD.ThingProperty = this.properties[propertyName];
+            if(property["@type"] == semantic["@type"]) {
+                // found property -> hand over to core functionality
+                return this.readProperty("TODO", options);
+            }
+        }
+        // no match found -> report it 
+        return Promise.reject("No semantic hit");
+    }
+
+    writeProperty(semantic: any, value: any, options?: WoT.InteractionOptions): Promise<void> {
+        return Promise.reject("Not yet implemented");
+    }
+
+    // ..., invokeAction, readAllProperties, readMultipleProperties, ...
+
+}
