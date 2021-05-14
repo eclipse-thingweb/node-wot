24a9eba782250f0e9c7f568916b2a3498b44d2df
Iori Mizutani <iori.mizutani@gmail.com>
2021-05-14 03:44:45 +0200

    fix: sync counter.ts with counter.js
    Signed-off-by: Iori Mizutani <iori.mizutani@gmail.com>

--- packages/examples/src/scripts/counter.ts
+++ packages/examples/src/scripts/counter.ts
@@ -187,7 +187,7 @@ WoT.produce({
 			lastChange = (new Date()).toISOString();
 			thing.emitEvent("change", count);
 		});
-		thing.setActionHandler("decrement", async (params, options) => {
+		thing.setActionHandler("reset", async (params, options) => {
 			console.log("Resetting count");
 			count = 0;
 			lastChange = (new Date()).toISOString();
