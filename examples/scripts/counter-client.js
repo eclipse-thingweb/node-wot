b2c0795168c370b58028f25d3b398954d68f8478
Iori Mizutani <iori.mizutani@gmail.com>
2021-05-14 05:04:58 +0200

    fix: examples/scripts/counter-client.js formIndex
    The formIndex meant for invoking the "decrement" action is the one with
    CoAP, hence the correct value should be 1

--- examples/scripts/counter-client.js
+++ examples/scripts/counter-client.js
@@ -31,8 +31,8 @@ WoTHelpers.fetch("coap://localhost:5683/counter").then(async (td) => {
         await thing.invokeAction("increment", undefined, { uriVariables: { 'step': 3 } });
         let inc2 = await thing.readProperty("count");
         console.info("count value after increment #2 (with step 3) is", inc2);
-        // decrement property with formIndex == 2
-        await thing.invokeAction("decrement", undefined, { formIndex: 2 });
+        // decrement property with formIndex == 1 (via CoAP binding)
+        await thing.invokeAction("decrement", undefined, { formIndex: 1 });
         let dec1 = await thing.readProperty("count");
         console.info("count value after decrement is", dec1);
     }
