1f95969267310fc273d4fd88c161e8bf6e257101
Jan Romann <jan.romann@uni-bremen.de>
2021-09-23 19:45:05 +0200

    feat(coap): align types with binding-templates

--- packages/binding-coap/src/coap.ts
+++ packages/binding-coap/src/coap.ts
@@ -27,14 +27,16 @@ export * from "./coap-client";
 export * from "./coaps-client-factory";
 export * from "./coaps-client";
 
-export class CoapForm extends Form {
-    public "coap:methodCode"?: number; // 1=0.01=GET, 2=0.02=POST, 3=0.03=PUT, 4=0.04=DELETE
-    public "coap:options"?: Array<CoapOption> | CoapOption;
+export class CoapOption {
+    public "cov:optionName": number;
+    public "cov:optionValue": any;
 }
 
-export class CoapOption {
-    public "coap:optionCode": number;
-    public "coap:optionValue": any;
+export type CoapMethodName = "GET" | "POST" | "PUT" | "DELETE" | "FETCH" | "PATCH" | "iPATCH";
+
+export class CoapForm extends Form {
+    public "cov:methodName"?: CoapMethodName;
+    public "cov:options"?: Array<CoapOption> | CoapOption;
 }
 
 export declare interface CoapRequestConfig {
