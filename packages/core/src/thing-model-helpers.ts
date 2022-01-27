b3311c12c4b60c081d26638d4791eeefc6a67213
reluc <relu.cri@gmail.com>
2022-01-27 16:56:51 +0100

    chore(core): fix license header in thing model helpers

--- packages/core/src/thing-model-helpers.ts
+++ packages/core/src/thing-model-helpers.ts
@@ -1,5 +1,5 @@
 /********************************************************************************
- * Copyright (c) 2018 - 2021 Contributors to the Eclipse Foundation
+ * Copyright (c) 2022 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
@@ -12,6 +12,7 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
  ********************************************************************************/
+
 
 import Ajv, { ValidateFunction, ErrorObject } from "ajv";
 import { LinkElement } from "wot-thing-description-types";
