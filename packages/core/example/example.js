d1ec05f30c9742746def9f54750ad6ab0cf362e9
Jan Romann <jan.romann@uni-bremen.de>
2022-05-26 15:50:33 +0200

    fixup! feat!: add proposal for new discovery API

--- packages/core/example/example.js
+++ packages/core/example/example.js
@@ -2,7 +2,7 @@ const { Servient } = require("@node-wot/core");
 const { HttpClientFactory } = require("@node-wot/binding-http");
 
 // Note: This example is just for testing/demonstration purposes and
-//       will eventually be moved to the examples package.
+//       will eventually be removed/moved to the examples package.
 async function discover() {
     const servient = new Servient();
     servient.addClientFactory(new HttpClientFactory());
