fc72c1ed2b3b0cec79a2d921e18c2af2a0bfd27e
reluc <relu.cri@gmail.com>
2023-01-16 18:15:05 +0100

    refactor(core): clean up octectstream encoding/decoding logic

--- packages/core/src/codecs/octetstream-codec.ts
+++ packages/core/src/codecs/octetstream-codec.ts
@@ -92,86 +92,92 @@ export default class OctetstreamCodec implements ContentCodec {
             case "boolean":
                 // true if any byte is non-zero
                 return !bytes.every((val) => val === 0);
-
             case "byte":
             case "short":
             case "int":
             case "integer":
-                switch (dataLength) {
-                    case 1:
-                        return signed ? bytes.readInt8(0) : bytes.readUInt8(0);
-
-                    case 2:
-                        return bigendian
-                            ? signed
-                                ? bytes.readInt16BE(0)
-                                : bytes.readUInt16BE(0)
-                            : signed
-                            ? bytes.readInt16LE(0)
-                            : bytes.readUInt16LE(0);
-
-                    case 4:
-                        return bigendian
-                            ? signed
-                                ? bytes.readInt32BE(0)
-                                : bytes.readUInt32BE(0)
-                            : signed
-                            ? bytes.readInt32LE(0)
-                            : bytes.readUInt32LE(0);
-
-                    default: {
-                        let result = 0;
-                        let negative;
-
-                        if (bigendian) {
-                            result = bytes.reduce((prev, curr) => prev << (8 + curr));
-                            negative = bytes.readInt8(0) < 0;
-                        } else {
-                            result = bytes.reduceRight((prev, curr) => prev << (8 + curr));
-                            negative = bytes.readInt8(dataLength - 1) < 0;
-                        }
-
-                        if (signed && negative) {
-                            result -= 1 << (8 * dataLength);
-                        }
-
-                        // warn about numbers being too big to be represented as safe integers
-                        if (!Number.isSafeInteger(result)) {
-                            warn("Result is not a safe integer");
-                        }
-
-                        return result;
-                    }
-                }
-
+                return this.integerToValue(bytes, { dataLength, bigendian, signed });
             case "float":
             case "double":
             case "number":
-                debug("qui", bigendian);
-                switch (dataLength) {
-                    case 2:
-                        return getFloat16(new DataView(bytes.buffer), bytes.byteOffset, !bigendian);
-                    case 4:
-                        return bigendian ? bytes.readFloatBE(0) : bytes.readFloatLE(0);
-
-                    case 8:
-                        return bigendian ? bytes.readDoubleBE(0) : bytes.readDoubleLE(0);
-
-                    default:
-                        throw new Error("Wrong buffer length for type 'number', must be 2, 4, 8, or is " + dataLength);
-                }
-
+                return this.numberToValue(bytes, { dataLength, bigendian });
             case "string":
                 return bytes.toString(parameters.charset as BufferEncoding);
-
             case "array":
             case "object":
                 throw new Error("Unable to handle dataType " + dataType);
-
             case "null":
                 return null;
             default:
                 throw new Error("Unable to handle dataType " + dataType);
+        }
+    }
+
+    private integerToValue(
+        bytes: Buffer,
+        options: { dataLength: number; bigendian: boolean; signed: boolean }
+    ): number {
+        const { dataLength, bigendian, signed } = options;
+        switch (dataLength) {
+            case 1:
+                return signed ? bytes.readInt8(0) : bytes.readUInt8(0);
+            case 2:
+                return bigendian
+                    ? signed
+                        ? bytes.readInt16BE(0)
+                        : bytes.readUInt16BE(0)
+                    : signed
+                    ? bytes.readInt16LE(0)
+                    : bytes.readUInt16LE(0);
+
+            case 4:
+                return bigendian
+                    ? signed
+                        ? bytes.readInt32BE(0)
+                        : bytes.readUInt32BE(0)
+                    : signed
+                    ? bytes.readInt32LE(0)
+                    : bytes.readUInt32LE(0);
+
+            default: {
+                let result = 0;
+                let negative;
+
+                if (bigendian) {
+                    result = bytes.reduce((prev, curr) => prev << (8 + curr));
+                    negative = bytes.readInt8(0) < 0;
+                } else {
+                    result = bytes.reduceRight((prev, curr) => prev << (8 + curr));
+                    negative = bytes.readInt8(dataLength - 1) < 0;
+                }
+
+                if (signed && negative) {
+                    result -= 1 << (8 * dataLength);
+                }
+
+                // warn about numbers being too big to be represented as safe integers
+                if (!Number.isSafeInteger(result)) {
+                    warn("Result is not a safe integer");
+                }
+
+                return result;
+            }
+        }
+    }
+
+    private numberToValue(bytes: Buffer, options: { dataLength: number; bigendian: boolean }): number {
+        const { dataLength, bigendian } = options;
+        switch (dataLength) {
+            case 2:
+                return getFloat16(new DataView(bytes.buffer), bytes.byteOffset, !bigendian);
+            case 4:
+                return bigendian ? bytes.readFloatBE(0) : bytes.readFloatLE(0);
+
+            case 8:
+                return bigendian ? bytes.readDoubleBE(0) : bytes.readDoubleLE(0);
+
+            default:
+                throw new Error("Wrong buffer length for type 'number', must be 2, 4, 8, or is " + dataLength);
         }
     }
 
@@ -185,7 +191,6 @@ export default class OctetstreamCodec implements ContentCodec {
         const bigendian = !parameters.byteSeq?.includes(Endianness.LITTLE_ENDIAN); // default to bigendian
         let signed = parameters.signed !== "false"; // if signed is undefined -> true (default)
         let length = parameters.length ? parseInt(parameters.length) : undefined;
-        let buf: Buffer;
 
         if (value === undefined) {
             throw new Error("Undefined value");
@@ -211,122 +216,137 @@ export default class OctetstreamCodec implements ContentCodec {
             case "byte":
             case "short":
             case "int":
-            case "integer": {
-                length = length ?? 4;
-                if (typeof value !== "number") {
-                    throw new Error("Value is not a number");
-                }
-
-                // warn about numbers being too big to be represented as safe integers
-                if (!Number.isSafeInteger(value)) {
-                    warn("Value is not a safe integer");
-                }
-                const limit = Math.pow(2, 8 * length) - 1;
-                // throw error on overflow
-                if (signed) {
-                    if (value < -limit || value >= limit) {
-                        throw new Error(
-                            "Integer overflow when representing signed " + value + " in " + length + " byte(s)"
-                        );
-                    }
-                } else {
-                    if (value < 0 || value >= limit) {
-                        throw new Error(
-                            "Integer overflow when representing unsigned " + value + " in " + length + " byte(s)"
-                        );
-                    }
-                }
-
-                buf = Buffer.alloc(length);
-                // Handle byte swapping
-                if (parameters.byteSeq?.includes("BYTE_SwAP") && length > 1) {
-                    buf.swap16();
-                }
-                switch (length) {
-                    case 1:
-                        signed ? buf.writeInt8(value, 0) : buf.writeUInt8(value, 0);
-                        break;
-
-                    case 2:
-                        bigendian
-                            ? signed
-                                ? buf.writeInt16BE(value, 0)
-                                : buf.writeUInt16BE(value, 0)
-                            : signed
-                            ? buf.writeInt16LE(value, 0)
-                            : buf.writeUInt16LE(value, 0);
-                        break;
-
-                    case 4:
-                        bigendian
-                            ? signed
-                                ? buf.writeInt32BE(value, 0)
-                                : buf.writeUInt32BE(value, 0)
-                            : signed
-                            ? buf.writeInt32LE(value, 0)
-                            : buf.writeUInt32LE(value, 0);
-                        break;
-
-                    default:
-                        if (signed && value < 0) {
-                            // convert to unsigned byte sequence
-                            value += 1 << (8 * length);
-                        }
-
-                        // use arithmetic instead of shift to cover more than 32 bits
-                        for (let i = 0; i < length; ++i) {
-                            const byte = value % 0x100;
-                            value /= 0x100;
-                            buf.writeInt8(byte, bigendian ? length - i - 1 : i);
-                        }
-                }
-
-                return buf;
-            }
+            case "integer":
+                return this.valueToInteger(value, {
+                    dataLength: length,
+                    bigendian,
+                    signed,
+                    byteSeq: parameters.byteSeq ?? "",
+                });
             case "float":
             case "number":
-                if (typeof value !== "number") {
-                    throw new Error("Value is not a number");
-                }
-
-                length = length ?? 8;
-                buf = Buffer.alloc(length);
-                // Handle byte swapping
-                if (parameters.byteSeq?.includes("BYTE_SwAP") && length > 1) {
-                    buf.swap16();
-                }
-                switch (length) {
-                    case 2:
-                        setFloat16(new DataView(buf.buffer), 0, value, !bigendian);
-                        break;
-                    case 4:
-                        bigendian ? buf.writeFloatBE(value, 0) : buf.writeFloatLE(value, 0);
-                        break;
-
-                    case 8:
-                        bigendian ? buf.writeDoubleBE(value, 0) : buf.writeDoubleLE(value, 0);
-                        break;
-
-                    default:
-                        throw new Error("Wrong buffer length for type 'number', must be 4 or 8, is " + length);
-                }
-
-                return buf;
-
+                return this.valueToNumber(value, { dataLength: length, bigendian, byteSeq: parameters.byteSeq ?? "" });
             case "string": {
                 const str = String(value);
                 return Buffer.from(str /*, params.charset */);
             }
-
             case "array":
             case "object":
             case "undefined":
                 throw new Error("Unable to handle dataType " + dataType);
-
             case "null":
                 return Buffer.alloc(0);
             default:
                 throw new Error("Unable to handle dataType " + dataType);
         }
+    }
+
+    private valueToInteger(
+        value: unknown,
+        options: { dataLength: number | undefined; bigendian: boolean; signed: boolean; byteSeq: string }
+    ): Buffer {
+        const length = options.dataLength ?? 4;
+        const { bigendian, signed, byteSeq } = options;
+
+        if (typeof value !== "number") {
+            throw new Error("Value is not a number");
+        }
+
+        // warn about numbers being too big to be represented as safe integers
+        if (!Number.isSafeInteger(value)) {
+            warn("Value is not a safe integer");
+        }
+        const limit = Math.pow(2, 8 * length) - 1;
+        // throw error on overflow
+        if (signed) {
+            if (value < -limit || value >= limit) {
+                throw new Error("Integer overflow when representing signed " + value + " in " + length + " byte(s)");
+            }
+        } else {
+            if (value < 0 || value >= limit) {
+                throw new Error("Integer overflow when representing unsigned " + value + " in " + length + " byte(s)");
+            }
+        }
+
+        const buf = Buffer.alloc(length);
+        // Handle byte swapping
+        if (byteSeq?.includes("BYTE_SwAP") && length > 1) {
+            buf.swap16();
+        }
+        switch (length) {
+            case 1:
+                signed ? buf.writeInt8(value, 0) : buf.writeUInt8(value, 0);
+                break;
+
+            case 2:
+                bigendian
+                    ? signed
+                        ? buf.writeInt16BE(value, 0)
+                        : buf.writeUInt16BE(value, 0)
+                    : signed
+                    ? buf.writeInt16LE(value, 0)
+                    : buf.writeUInt16LE(value, 0);
+                break;
+
+            case 4:
+                bigendian
+                    ? signed
+                        ? buf.writeInt32BE(value, 0)
+                        : buf.writeUInt32BE(value, 0)
+                    : signed
+                    ? buf.writeInt32LE(value, 0)
+                    : buf.writeUInt32LE(value, 0);
+                break;
+
+            default:
+                if (signed && value < 0) {
+                    // convert to unsigned byte sequence
+                    value += 1 << (8 * length);
+                }
+
+                // use arithmetic instead of shift to cover more than 32 bits
+                for (let i = 0; i < length; ++i) {
+                    const byte = value % 0x100;
+                    value /= 0x100;
+                    buf.writeInt8(byte, bigendian ? length - i - 1 : i);
+                }
+        }
+
+        return buf;
+    }
+
+    private valueToNumber(
+        value: unknown,
+        options: { dataLength: number | undefined; bigendian: boolean; byteSeq: string }
+    ): Buffer {
+        if (typeof value !== "number") {
+            throw new Error("Value is not a number");
+        }
+
+        const length = options.dataLength ?? 8;
+        const { bigendian, byteSeq } = options;
+        const buf = Buffer.alloc(length);
+
+        // Handle byte swapping
+        if (byteSeq && length > 1) {
+            buf.swap16();
+        }
+        switch (length) {
+            case 2:
+                setFloat16(new DataView(buf.buffer), 0, value, !bigendian);
+                break;
+            case 4:
+                bigendian ? buf.writeFloatBE(value, 0) : buf.writeFloatLE(value, 0);
+                break;
+
+            case 8:
+                bigendian ? buf.writeDoubleBE(value, 0) : buf.writeDoubleLE(value, 0);
+                break;
+
+            default:
+                throw new Error("Wrong buffer length for type 'number', must be 4 or 8, is " + length);
+        }
+
+        return buf;
     }
 }
