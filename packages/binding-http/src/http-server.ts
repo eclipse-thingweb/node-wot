a1ff15677b201bf61e96765ee739b337c2dcf7ea
reluc <relu.cri@gmail.com>
2023-05-17 17:04:14 +0200

    fix(binding-http): prefer application/td+json when is possibile instead of text/html

--- packages/binding-http/src/http-server.ts
+++ packages/binding-http/src/http-server.ts
@@ -570,7 +570,16 @@ export default class HttpServer implements ProtocolServer {
 
                 return acceptValue;
             })
-            .filter((acceptValue) => contentSerdes.isSupported(acceptValue));
+            .filter((acceptValue) => contentSerdes.isSupported(acceptValue))
+            .sort((a, b) => {
+                // weight function last places weight more than first: application/td+json > application/json > text/html
+                const aWeight =
+                    ["text/html", "application/json", "application/td+json"].findIndex((value) => value === a) ?? 0;
+                const bWeight =
+                    ["text/html", "application/json", "application/td+json"].findIndex((value) => value === b) ?? 0;
+
+                return bWeight - aWeight;
+            });
 
         if (filteredAcceptValues.length > 0) {
             const contentType = filteredAcceptValues[0];
