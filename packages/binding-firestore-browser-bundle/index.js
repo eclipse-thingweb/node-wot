44f68f8bf4284fae3af94a14d37c3dba90703013
danielpeintner <daniel.peintner@gmail.com>
2023-06-13 10:45:32 +0200

    chore: remove binding-firestore (#1021)
    * remove firestore
    
    * fix: Type 'string[]' is not assignable to type '[] | RegExpMatchArray'.
    
    * use @types/node 16.18.35
    
    * set "typescript" to version "4.7.4" to avoid eslint issues
    
    NOTE: SUPPORTED TYPESCRIPT VERSIONS: >=3.3.1 <4.5.0
    but 4.7.4 seems to work just fine

--- packages/binding-firestore-browser-bundle/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const BindingFirestore = require("@node-wot/binding-firestore");
-
-if (typeof window !== "undefined") {
-    window.BindingFirestore = BindingFirestore;
-} else if (typeof module !== "undefined" && module.exports) {
-    module.exports = BindingFirestore;
-}
