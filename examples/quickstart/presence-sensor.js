9de884f9d0c1ff5f2eb48bb308df9b39afd35b26
Ege Korkan <egekorkan@gmail.com>
2023-09-05 00:16:04 +0300

    examples: copy js files to root examples folder

---
+++ examples/quickstart/presence-sensor.js
@@ -0,0 +1,59 @@
+"use strict";
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+// This is an example Thing script which is a simple presence detector
+// It fires an event when it detects a person (mocked as every 5 second)
+const core_1 = require("@node-wot/core");
+const binding_mqtt_1 = require("@node-wot/binding-mqtt");
+// create Servient add MQTT binding with port configuration
+const servient = new core_1.Servient();
+servient.addServer(new binding_mqtt_1.MqttBrokerServer({ uri: "mqtt://test.mosquitto.org" }));
+servient.start().then((WoT) => {
+    WoT.produce({
+        title: "PresenceSensor",
+        description: "Thing that can detect presence of human nearby",
+        support: "https://github.com/eclipse-thingweb/node-wot/",
+        "@context": "https://www.w3.org/2022/wot/td/v1.1",
+        events: {
+            presenceDetected: {
+                title: "Presence Detected",
+                description: "An event that is emitted when a person is detected in the room. It is mocked and emitted every 5 seconds",
+                data: {
+                    type: "number",
+                    title: "Distance",
+                    minimum: 55,
+                    maximum: 1200,
+                },
+            },
+        },
+    })
+        .then((thing) => {
+        console.log("Produced " + thing.getThingDescription().title);
+        // expose the thing
+        thing.expose().then(() => {
+            console.info(thing.getThingDescription().title + " ready");
+            // mocking the detection with an event sent every 5 seconds, with a random distance
+            setInterval(() => {
+                const distance = Math.random() * (1200 - 55) + 55;
+                thing.emitEvent("presenceDetected", distance);
+                console.info("Emitted presence with distance ", distance);
+            }, 5000);
+        });
+    })
+        .catch((e) => {
+        console.log(e);
+    });
+});
---
+++ examples/quickstart/simple-coffee-machine.js
@@ -0,0 +1,134 @@
+"use strict";
+/********************************************************************************
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0, or the W3C Software Notice and
+ * Document License (2015-05-13) which is available at
+ * https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document.
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR W3C-20150513
+ ********************************************************************************/
+Object.defineProperty(exports, "__esModule", { value: true });
+// This is an example Thing script which is a simple coffee machine.
+// You can order coffee and see the status of the resources
+const core_1 = require("@node-wot/core");
+const binding_http_1 = require("@node-wot/binding-http");
+// create Servient add HTTP binding with port configuration
+const servient = new core_1.Servient();
+servient.addServer(new binding_http_1.HttpServer({
+    port: 8081,
+}));
+let waterAmount = 100;
+let beansAmount = 100;
+let milkAmount = 100;
+// promisify timeout since it does not return a promise
+function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+servient.start().then((WoT) => {
+    WoT.produce({
+        title: "Coffee Machine",
+        description: "A simple coffee machine that can be interacted over the Internet",
+        support: "https://github.com/eclipse-thingweb/node-wot/",
+        "@context": "https://www.w3.org/2022/wot/td/v1.1",
+        properties: {
+            resources: {
+                readOnly: true,
+                observable: true,
+                type: "object",
+                properties: {
+                    water: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: 100,
+                    },
+                    beans: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: 100,
+                    },
+                    milk: {
+                        type: "integer",
+                        minimum: 0,
+                        maximum: 100,
+                    },
+                },
+            },
+        },
+        actions: {
+            brew: {
+                synchronous: true,
+                input: {
+                    type: "string",
+                    enum: ["espresso", "cappuccino", "americano"],
+                },
+            },
+        },
+    })
+        .then((thing) => {
+        console.log("Produced " + thing.getThingDescription().title);
+        thing.setPropertyReadHandler("resources", async () => {
+            return {
+                water: waterAmount,
+                beans: beansAmount,
+                milk: milkAmount,
+            };
+        });
+        thing.setActionHandler("brew", async (params, options) => {
+            const coffeeType = await params.value();
+            console.info("received coffee order of ", coffeeType);
+            if (coffeeType === "espresso") {
+                if (waterAmount <= 10 || beansAmount <= 10) {
+                    throw new Error("Not enough water or beans");
+                }
+                else {
+                    await timeout(1000);
+                    waterAmount = waterAmount - 10;
+                    beansAmount = beansAmount - 10;
+                    thing.emitPropertyChange("resources");
+                    return undefined;
+                }
+            }
+            else if (coffeeType === "cappuccino") {
+                if (waterAmount <= 20 || beansAmount <= 25 || milkAmount <= 15) {
+                    throw new Error("Not enough water or beans");
+                }
+                else {
+                    await timeout(2000);
+                    waterAmount = waterAmount - 15;
+                    beansAmount = beansAmount - 20;
+                    milkAmount = milkAmount - 10;
+                    thing.emitPropertyChange("resources");
+                    return undefined;
+                }
+            }
+            else if (coffeeType === "americano") {
+                if (waterAmount <= 35 || beansAmount <= 10) {
+                    throw new Error("Not enough water or beans");
+                }
+                else {
+                    await timeout(2000);
+                    waterAmount = waterAmount - 30;
+                    beansAmount = beansAmount - 10;
+                    thing.emitPropertyChange("resources");
+                    return undefined;
+                }
+            }
+            else {
+                throw new Error("Wrong coffee input");
+            }
+        });
+        // expose the thing
+        thing.expose().then(() => {
+            console.info(thing.getThingDescription().title + " ready");
+        });
+    })
+        .catch((e) => {
+        console.log(e);
+    });
+});
