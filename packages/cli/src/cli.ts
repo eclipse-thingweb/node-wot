b62e3fdebf13dd3d354d50fcb0f15482cf7c7854
Jan Romann <jan.romann@uni-bremen.de>
2023-10-19 10:08:22 +0200

    fixup! refactor: enable strict-boolean-expressions globally
    Co-authored-by: Cristiano Aguzzi <relu91@users.noreply.github.com>

--- packages/cli/src/cli.ts
+++ packages/cli/src/cli.ts
@@ -311,7 +311,7 @@ const runScripts = async function (servient: DefaultServient, scripts: Array<str
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     if (debug && debug.shouldBreak) {
         // Activate inspector only if is not already opened and wait for the debugger to attach
-        inspector.url() != null && inspector.open(debug.port, debug.host, true);
+        inspector.url() == null && inspector.open(debug.port, debug.host, true);
 
         // Set a breakpoint at the first line of of first script
         // the breakpoint gives time to inspector clients to set their breakpoints
