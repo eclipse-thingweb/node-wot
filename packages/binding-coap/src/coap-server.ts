a640d6cfb8247f386d89d86e6a5f0fe8fb13625b
Jan Romann <jan.romann@uni-bremen.de>
2023-12-11 22:52:06 +0100

    fixup! fix(coap-server): ignore incoming messages with an invalid source port

--- packages/binding-coap/src/coap-server.ts
+++ packages/binding-coap/src/coap-server.ts
@@ -435,7 +435,9 @@ export default class CoapServer implements ProtocolServer {
     }
 
     private async handleRequest(req: IncomingMessage, res: OutgoingMessage) {
-        if (req.rsinfo.port === 0) {
+        const sourcePort = req.rsinfo.port;
+        const hasInvalidPortRange =  sourcePort < 1 || sourcePort > 65535;
+        if (hasInvalidPortRange) {
             // Ignore requests with an invalid source port
             // See https://github.com/eclipse-thingweb/node-wot/issues/1182
             return;
