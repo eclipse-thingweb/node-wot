7cc777fd58fd534f2bae031a24a9bf9fbf1cc083
Jan Romann <jan.romann@uni-bremen.de>
2023-12-21 12:57:31 +0100

    fixup! feat(core): implement `exploreDirectory` method

--- packages/core/src/helpers.ts
+++ packages/core/src/helpers.ts
@@ -55,7 +55,6 @@ const ajv = new Ajv({ strict: false })
         "date-time",
         /^\d\d\d\d-[0-1]\d-[0-3]\d[t\s](?:[0-2]\d:[0-5]\d:[0-5]\d|23:59:60)(?:\.\d+)?(?:z|[+-]\d\d(?::?\d\d)?)$/
     );
-
 export default class Helpers implements Resolver {
     static tsSchemaValidator = ajv.compile(Helpers.createExposeThingInitSchema(tdSchema)) as ValidateFunction;
 
