eca24fdafc8b8deed17747c5be208705308468d5
danielpeintner <daniel.peintner@gmail.com>
2025-02-18 10:24:53 +0100

    refactor: make linter happy
    error  'paths' is never reassigned. Use 'const' instead  prefer-const

--- packages/core/test/HelpersTest.ts
+++ packages/core/test/HelpersTest.ts
@@ -202,7 +202,7 @@ class HelperTest {
 
     @test "should generate unique names over and over again"() {
         let urlPath = "bla";
-        let paths = [urlPath];
+        const paths = [urlPath];
         for (let i = 0; i < 5; i++) {
             urlPath = Helpers.generateUniqueName(urlPath);
             expect(paths.includes(urlPath)).to.equal(false);
