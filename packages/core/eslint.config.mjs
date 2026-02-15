6a44aea41a24408a1e5dc7e200b4c73e68825d1a
ViRUS-0-0 <mrvanwariomg@gmail.com>
2026-02-15 19:03:26 +0530

    fix(core): add mjs files to format script and apply consistent quoting in eslint config.

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -1,10 +1,10 @@
-import rootConfig from '../../eslint.config.mjs';
+import rootConfig from "../../eslint.config.mjs";
 
 export default [
-  ...rootConfig,
-  {
-    rules: {
-      '@typescript-eslint/no-unnecessary-condition': 'warn'
-    }
-  }
+    ...rootConfig,
+    {
+        rules: {
+            " @typescript-eslint/no-unnecessary-condition": "warn",
+        },
+    },
 ];
